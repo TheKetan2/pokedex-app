c6755cce90fd60308db313f81fa15d042234160f
Ketan <ketantoo@gmail.com>
2020-02-21 10:50:03 +0530

    Pokecard component styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import "./Pokecard.css";
+
 const POKE_API =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
@@ -9,6 +11,8 @@ export default class Pokecard extends Component {
       <div className="Pokecard">
         <h1>{this.props.name}</h1>
         <img src={imgSrc} alt={this.props.name} />
+        <div>Type: {this.props.type}</div>
+        <div>Exp: {this.props.exp}</div>
       </div>
     );
   }
