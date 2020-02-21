70cfac88b6838d9998152a91e926dbdcb0c09193
Ketan <ketantoo@gmail.com>
2020-02-21 10:46:35 +0530

    Pokecard component taking shape

---
+++ src/Pokecard.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+const POKE_API =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+export default class Pokecard extends Component {
+  render() {
+    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    return (
+      <div className="Pokecard">
+        <h1>{this.props.name}</h1>
+        <img src={imgSrc} alt={this.props.name} />
+      </div>
+    );
+  }
+}
