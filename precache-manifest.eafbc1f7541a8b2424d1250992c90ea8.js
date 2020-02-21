9f91ad314d54d9cc68be296f8004274434a5fb3f
Ketan <ketantoo@gmail.com>
2020-02-21 13:32:12 +0530

    Updates

---
+++ precache-manifest.eafbc1f7541a8b2424d1250992c90ea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05805c91b21fbaa20920125755055fdb",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "0639da9a7ad78363b5d6",
+    "url": "/react-gh-pages/static/css/main.1610811a.chunk.css"
+  },
+  {
+    "revision": "d802d12e04ec612c64bd",
+    "url": "/react-gh-pages/static/js/2.2664b81e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-gh-pages/static/js/2.2664b81e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0639da9a7ad78363b5d6",
+    "url": "/react-gh-pages/static/js/main.03b678c1.chunk.js"
+  },
+  {
+    "revision": "f282d062d47455df53ee",
+    "url": "/react-gh-pages/static/js/runtime-main.f7d4f309.js"
+  }
+]);
